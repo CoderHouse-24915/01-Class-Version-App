1ac8ea8807a2913f5967f569904117dd17853ab4
DanielDiSalvo <danieldi_salvo@hotmail.com>
2022-01-31 18:35:51 -0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+import img from './logo.png';
+
+const Header = (props) => {
+	return (
+		<div className='Header'>
+			<img src={img} alt='logo-coder-house' />
+			<h1>{props.title}</h1>
+			<h2>{props.subtitle}</h2>
+		</div>
+	);
+};
+
+export default Header;
